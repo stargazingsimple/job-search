2f18ff3f01f48b32e040f81f9ed5c64cccf06d57
stargazingsimple <stargazingsimple@gmail.com>
2026-04-07 10:54:59 +0300

    lint-staged configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,17 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
+import { includeIgnoreFile } from '@eslint/compat'
+import { fileURLToPath } from 'node:url'
 import globals from 'globals'
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
 import skipFormatting from 'eslint-config-prettier/flat'
 
+const gitIgnorePath = fileURLToPath(new URL('.gitignore', import.meta.url))
+
 export default defineConfig([
+  includeIgnoreFile(gitIgnorePath, 'Imported .gitignore patterns'),
+
   {
     name: 'app/files-to-lint',
     files: ['**/*.{vue,js,mjs,jsx}'],
