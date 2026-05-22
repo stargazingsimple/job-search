16e8d4c405484fea226543451fa70637eb992584
stargazingsimple <stargazingsimple@gmail.com>
2026-05-22 20:28:16 +0300

    typing api

--- src/api/jobs/jobs.ts
+++ src/api/jobs/jobs.ts
@@ -1,5 +1,6 @@
 import { HTTP } from '@/api'
+import type { Job } from '@/api/jobs/types.ts'
 
 export const getJobs = async () => {
-  return await HTTP.get('/jobs.json')
+  return await HTTP.get<Job[]>('/jobs.json')
 }
